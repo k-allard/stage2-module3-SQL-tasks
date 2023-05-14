7ded10ad4acb754bcd12021c1148a9a5dbb6bd66
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-15 00:15:01 +0400

    Done Lesson 11

---
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -0,0 +1,4 @@
+update subject set grade = 5 where name = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+update student set groupNumber = 8 where name = 'Tremaine Worvill';
+update payment set amount = 500, student_id = 2 where payment_date > DATE '2021-01-01' and type_id = 2;
+update mark set mark = 2 where subject_id = 315;
