cdf2bafbb1004bf942ec286c63b9d1921152c97d
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-15 20:31:18 +0400

    Fix ont more test

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
-select distinct Student.* from Student join mark on (mark.student_id = student.id) where mark.mark > 8;
+select student.* from Student join mark on (mark.student_id = student.id) group by student.id having avg(mark.mark) > 8;
 select Student.id, Student.name from Student join mark on (mark.student_id = student.id) group by Student.id, Student.name having min(mark.mark) > 7;
 select Student.id, Student.name from Student join payment on (payment.student_id = student.id) where year(payment.payment_date) = 2019 group by Student.id, Student.name having count(student.id) > 2;
