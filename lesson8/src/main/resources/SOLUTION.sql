7317bbdf4b26eeace1a1703fcc321daa2c84568f
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-15 20:20:04 +0400

    Fix ont more test

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 select max(birthday) from student;
 select min(payment_date) from payment;
-select avg(mark) from mark;
+select avg(mark) from mark join subject on (mark.subject_id = subject.id)  where subject.name = 'Math';
 select min(amount) from payment join paymenttype on (payment.type_id = paymenttype.id) where paymenttype.name = 'WEEKLY';
