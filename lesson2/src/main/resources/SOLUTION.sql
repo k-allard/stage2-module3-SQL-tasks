57d41db74b4401ea7292e4c37bb595d655ba4e69
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-15 18:20:24 +0400

    Fix some tests

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -11,33 +11,33 @@ insert into student (id, name, birthday, groupnumber) values (10, 'Mary', '1974-
 insert into student (id, name, birthday, groupnumber) values (11, 'Stella', '1974-03-09', 4);
 insert into student (id, name, birthday, groupnumber) values (12, 'Helen', '1974-03-09', 5);
 insert into student (id, name, birthday, groupnumber) values (13, 'Rose', '1974-03-09', 5);
-INSERT INTO subject(id, name, grade)
-VALUES (1, 'art', 1),
-       (2, 'music', 1),
-       (3, 'geography', 2),
-       (4, 'history', 2),
-       (5, 'PE', 3),
-       (6, 'math', 3),
-       (7, 'science', 4),
-       (8, 'IT', 4),
-       (9, 'dancing', 5),
-       (10, 'singing', 5);
-INSERT INTO paymenttype(id, name)
-VALUES (1, 'DAILY'),
-       (2, 'WEEKLY'),
-       (3, 'MONTHLY');
-INSERT INTO payment(id, type_id, amount, payment_date, student_id)
-VALUES (1, 2, 150, '2023-03-09', 1),
-       (2, 3, 150, '2023-03-09', 4),
-       (3, 2, 150, '2023-03-09', 7),
-       (4, 1, 150, '2023-03-09', 5),
-       (5, 3, 150, '2023-03-09', 11),
-       (6, 3, 150, '2023-03-09', 12);
-INSERT INTO mark(id, student_id, subject_id, mark)
-VALUES (1, 2, 1, 8),
-       (2, 4, 4, 5),
-       (3, 5, 3, 9),
-       (4, 8, 6, 4),
-       (5, 9, 5, 9),
-       (6, 13, 7, 10),
-       (7, 12, 8, 10);
+
+INSERT INTO subject (id, name, grade) VALUES (1, 'art', 1);
+INSERT INTO subject (id, name, grade) VALUES (2, 'music', 1);
+INSERT INTO subject (id, name, grade) VALUES (3, 'geography', 2);
+INSERT INTO subject (id, name, grade) VALUES (4, 'history', 2);
+INSERT INTO subject (id, name, grade) VALUES (5, 'PE', 3);
+INSERT INTO subject (id, name, grade) VALUES (6, 'math', 3);
+INSERT INTO subject (id, name, grade) VALUES (7, 'science', 4);
+INSERT INTO subject (id, name, grade) VALUES (8, 'IT', 4);
+INSERT INTO subject (id, name, grade) VALUES (9, 'dancing', 5);
+INSERT INTO subject (id, name, grade) VALUES (10, 'singing', 5);
+
+insert into paymenttype (id, name) values ( 1, 'DAILY');
+insert into paymenttype (id, name) values (2,'WEEKLY');
+insert into paymenttype (id, name) values (3, 'MONTHLY');
+
+INSERT INTO payment (id, type_id, amount, payment_date, student_id) VALUES (1, 2, 150, '2023-03-09', 1);
+INSERT INTO payment (id, type_id, amount, payment_date, student_id) VALUES (2, 3, 150, '2023-03-09', 4);
+INSERT INTO payment (id, type_id, amount, payment_date, student_id) VALUES(3, 2, 150, '2023-03-09', 7);
+INSERT INTO payment (id, type_id, amount, payment_date, student_id) VALUES (4, 1, 150, '2023-03-09', 5);
+INSERT INTO payment (id, type_id, amount, payment_date, student_id) VALUES(5, 3, 150, '2023-03-09', 11);
+INSERT INTO payment (id, type_id, amount, payment_date, student_id) VALUES (6, 3, 150, '2023-03-09', 12);
+
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (1, 2, 1, 8);
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (2, 4, 4, 5);
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (3, 5, 3, 9);
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (4, 8, 6, 4);
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (5, 9, 5, 9);
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (6, 13, 7, 10);
+INSERT INTO mark (id, student_id, subject_id, mark) VALUES (7, 12, 8, 10);
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -4,4 +4,4 @@ SELECT * from student where groupnumber = 10 and birthday > DATEADD(YEAR, -20,  
 SELECT * from student where name like 'Mike %' or groupnumber in(4, 5, 6);
 SELECT * FROM payment where payment_DATE > DATEADD(Month, -8,  PARSEDATETIME('20230513', 'yyyyMMdd'));
 SELECT * from student where name like 'A%';
-SELECT * from student where name like 'Tommie %' and groupnumber = 2 or name like 'Isacco %' and groupnumber = 7;
+SELECT * from student where name like 'Roxi %' and groupnumber = 4 or name like 'Tallie %' and groupnumber = 9;
