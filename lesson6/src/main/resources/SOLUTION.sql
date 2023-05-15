8b58911843217fc2f4761e6e8d52ac40ccdbe5f6
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-15 20:02:23 +0400

    Fix ont more test

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT PAYMENT.* FROM PAYMENT join PAYMENTTYPE on (payment.type_id = paymenttype.id) where paymenttype.name = 'MONTHLY';
 SELECT MARK.* FROM MARK join subject on (mark.subject_id = subject.id) where subject.name = 'Art';
-select student.* from payment join paymenttype on (payment.type_id = paymenttype.id) join student on (payment.student_id = student.id) where paymenttype.name = 'WEEKLY';
+select distinct student.id from payment join paymenttype on (payment.type_id = paymenttype.id) join student on (payment.student_id = student.id) where paymenttype.name = 'WEEKLY';
 select student.* from mark join subject on (mark.subject_id = subject.id) join student on (mark.student_id = student.id) where subject.name = 'Math';
